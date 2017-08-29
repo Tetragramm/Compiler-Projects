8552da28538647536b1eea9b76de7d990e9eac72
Tetragramm <gaussgun+git@gmail.com>
2017-08-28 20:36:55 -0500

    Project 0, read in lines and output the lines with line numbers in new file.

--- Lexical Analyzer/Lexical Analyzer/LexicalAnalyzer.cpp
+++ Lexical Analyzer/Lexical Analyzer/LexicalAnalyzer.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
-
+#include <fstream>
+#include <sstream>
 
 int main()
 {
+	using namespace std;
+
+	ifstream sourceFile("TestPascal.txt");
+	ofstream listingFile("TestListing.txt");
+	string line;
+	int lineCount = 0;
+	bool complete = false;
+	while(!complete)
+	{
+		getline(sourceFile, line);
+		listingFile << lineCount << " " << line << "\n";
+		lineCount++;
+		complete = sourceFile.eof();
+	}
+
     return 0;
 }
 
